0f3360c26085a6955a6b95eac4b23320d352b999
nipowow <nicolas.poggio@davinci.edu.ar>
2025-04-23 21:07:20 -0300

    Barco y encBarcos en proceso
    Correcciones y terminando los metodos del encargado de barcos

--- src/Barco.java
+++ src/Barco.java
@@ -1,17 +1,29 @@
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 
 public class Barco {
-
+	private LinkedList<Barco> Barco = new LinkedList<Barco>();
 	private String nombre;
-	private LocalDateTime horaEntrada;
-	private LocalDateTime horaSalida;
+	private String fechaEntrada;
+	private String fechaSalida;
+	private String horaEntrada;
+	private String horaSalida;
 	private int capacidadCarga;
 	private double tarifa;
 	private double impuesto;
 	
-	public Barco(String nombre, LocalDateTime horaEntrada, LocalDateTime horaSalida, int capacidadCarga, double tarifa,
-			double impuesto) {
+	public Barco(String nombre, String fechaEntrada,String horaEntrada,int capacidadCarga) {
 		this.nombre = nombre;
+		this.fechaEntrada = fechaEntrada;
+		this.horaEntrada = horaEntrada;
+		this.capacidadCarga = capacidadCarga;
+	}
+	public Barco(LinkedList<Barco> barco, String nombre, String fechaEntrada, String fechaSalida, String horaEntrada,
+			String horaSalida, int capacidadCarga, double tarifa, double impuesto) {
+		Barco = barco;
+		this.nombre = nombre;
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
 		this.horaEntrada = horaEntrada;
 		this.horaSalida = horaSalida;
 		this.capacidadCarga = capacidadCarga;
@@ -19,24 +31,45 @@ public class Barco {
 		this.impuesto = impuesto;
 	}
 	
+
+	public LinkedList<Barco> getBarco() {
+		return Barco;
+	}
+	public void setBarco(LinkedList<Barco> barco) {
+		Barco = barco;
+	}
+	public String getFechaEntrada() {
+		return fechaEntrada;
+	}
+	public void setFechaEntrada(String fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+	public String getFechaSalida() {
+		return fechaSalida;
+	}
+	public void setFechaSalida(String fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+	public String getHoraEntrada() {
+		return horaEntrada;
+	}
+	public void setHoraEntrada(String horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+	public String getHoraSalida() {
+		return horaSalida;
+	}
+	public void setHoraSalida(String horaSalida) {
+		this.horaSalida = horaSalida;
+	}
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public LocalDateTime getHoraEntrada() {
-		return horaEntrada;
-	}
-	public void setHoraEntrada(LocalDateTime horaEntrada) {
-		this.horaEntrada = horaEntrada;
-	}
-	public LocalDateTime getHoraSalida() {
-		return horaSalida;
-	}
-	public void setHoraSalida(LocalDateTime horaSalida) {
-		this.horaSalida = horaSalida;
-	}
+
+
 	public int getCapacidadCarga() {
 		return capacidadCarga;
 	}
--- src/encBarcos.java
+++ src/encBarcos.java
@@ -1,17 +1,86 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
 
 public class encBarcos extends usuario {
-	
+	private LinkedList<Barco> barcos = new LinkedList<Barco>();
 	public encBarcos(String nombre, String contraseña, LocalDate fechanacimiento, int telefono, String direccion) {
-		super(nombre, contraseña, fechanacimiento, telefono, direccion);
+		super(nombre, contraseña, fechanacimiento, telefono, direccion, direccion);
 	}
-	public void registrarBarco{
-		
+	public void registrarBarco(){
+		String nombre = validarCaracteres("Ingrese el nombre del barco");
+		String fechaEntrada = JOptionPane.showInputDialog("Ingrese La fecha de entrada del barco");
+		String horaEntrada = JOptionPane.showInputDialog("Ingrese la hora de entrada del barco");
+		int capacidadcarga = validarNumeros("Ingrese la capacidad de carga");
+		Barco nuevoBarco = new Barco(nombre,fechaEntrada,horaEntrada,capacidadcarga);
+		barcos.add(nuevoBarco);
+		JOptionPane.showMessageDialog(null, "Nombre"+nombre+"\n"+"Hora de Entrada"+horaEntrada+"\n"+"Capacidad de carga:"+capacidadcarga);
 	}
-	public void registrarsalidaBarco{
-		
+	public void registrarsalidaBarco(){
+		String nombre = validarCaracteres("Ingrese el nombre del barco que va a salir:");
+		 Barco barcoEncontrado = null;
+		    for (Barco b : barcos) {
+		        if (b.getNombre().equalsIgnoreCase(nombre)) {
+		            barcoEncontrado = b;
+		            break;
+		        }
+		        
+		    }
+		    if (barcoEncontrado == null) {
+		        JOptionPane.showMessageDialog(null, "No se encontró un barco con ese nombre.");
+		        return;
+		    }
+
+			String fechaSalida = JOptionPane.showInputDialog("Ingrese La fecha de Salida del barco");
+		    String horaSalida = JOptionPane.showInputDialog("Ingrese la hora de salida del barco");
+		    barcoEncontrado.setHoraSalida(horaSalida);
 	}
-	public void calcularTarifaImpuesto{
+	public void calcularTarifaImpuesto(){
 	
+	}
+	public void verBarcos() {
+		if (barcos.isEmpty()) {
+	        JOptionPane.showMessageDialog(null, "No hay barcos registrados.");
+	        return;
+	    }
+
+	    for (Barco b : barcos) {
+	        String datos = "Nombre: " + b.getNombre() + "\n"
+	                     + "Hora de entrada: " + b.getHoraEntrada() + "\n"
+	                     + "Hora de salida: " + (b.getHoraSalida() != null ? b.getHoraSalida() : "No registrada") + "\n"
+	                     + "Capacidad de carga: " + b.getCapacidadCarga() + "\n";
+
+	        JOptionPane.showMessageDialog(null, datos);
+	    }
+	}
+	public String validarCaracteres(String mensaje) {
+		String palabra = "";
+		while (palabra.contains("1") || palabra.equals(" ") || palabra.contains("2") || palabra.contains("3")
+				|| palabra.contains("4") || palabra.contains("5") || palabra.contains("6") || palabra.contains("7")
+				|| palabra.contains("8") || palabra.contains("9") || palabra.contains("0") || palabra.isEmpty()) {
+			palabra = JOptionPane.showInputDialog(mensaje);
+		}
+		return palabra;
+	}
+	public int validarNumeros(String mensaje) {
+		boolean flag;
+		String num = "";
+		do {
+			flag = true;
+			num = JOptionPane.showInputDialog(mensaje);
+			while (num.isEmpty()) {
+				num = JOptionPane.showInputDialog(mensaje);
+			}
+			for (int i = 0; i < num.length(); i++) {
+				if (!Character.isDigit(num.charAt(i))) {
+					flag = false;
+					break;
+				}
+			}
+		} while (!flag);
+
+		return Integer.parseInt(num);
 	}
 }
